741ed65fcc82a15ce5798cf90a823d8f4b4814e0
unknown <rojandhimal1@gmail.com>
2021-02-27 13:36:05 +0545

    adding footer

---
+++ src/components/Banner.js
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+import "./Banner.css";
+import { Button } from "@material-ui/core";
+import Search from "./Search";
+
+function Banner() {
+  const [showSearch, setShowSearch] = useState(false);
+  return (
+    <div className="banner">
+      <div className="banner__search">
+        <Button
+          onClick={() => {
+            setShowSearch(!showSearch);
+          }}
+          variant="outlined"
+          className="banner__searchButton"
+        >
+          {showSearch ? "Hide" : "Search Dates"}
+        </Button>
+        {showSearch && <Search />}
+      </div>
+      <div className="banner__info">
+        <h1>Get out and strech your Imagination</h1>
+        <h5>
+          Plan a different kind of gateway to uncover the hidden gems near you.
+        </h5>
+        <Button variant="outlined">Explore Nearby</Button>
+      </div>
+    </div>
+  );
+}
+
+export default Banner;
---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <div>
+      <p>
+        © 2021 Airbnb clone! No rights reserved - this is a demo! -Rojan Dhimal
+      </p>
+      <p>Privacy · Terms · Sitemap · Company Details</p>
+    </div>
+  );
+}
+
+export default Footer;
